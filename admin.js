b1a4a7eefbefa128dab360b6a6d20410a43677d4
alexshn88888-sudo <alexshn88888@gmail.com>
2026-05-22 16:55:43 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,39 +1,17 @@
+function addProduct() {
+  let products = JSON.parse(localStorage.getItem("products")) || [];
 
-function getProducts(){
-  return JSON.parse(localStorage.getItem('products') || '[]');
+  const newProduct = {
+    id: Date.now(),
+    name: document.getElementById("name").value,
+    price: document.getElementById("price").value,
+    description: document.getElementById("desc").value,
+    image: document.getElementById("img").value
+  };
+
+  products.push(newProduct);
+
+  localStorage.setItem("products", JSON.stringify(products));
+
+  alert("Товар добавлен!");
 }
-
-function saveProducts(p){
-  localStorage.setItem('products', JSON.stringify(p));
-  render();
-}
-
-function addProduct(){
-  const name = document.getElementById('name').value;
-  const price = document.getElementById('price').value;
-  const desc = document.getElementById('desc').value;
-
-  const products = getProducts();
-  products.push({name, price, desc});
-  saveProducts(products);
-}
-
-function deleteProduct(i){
-  const products = getProducts();
-  products.splice(i,1);
-  saveProducts(products);
-}
-
-function render(){
-  const list = document.getElementById('list');
-  const products = getProducts();
-
-  list.innerHTML = products.map((p,i)=>
-    `<div>
-      <b>${p.name}</b> - ${p.price}
-      <button onclick="deleteProduct(${i})">Удалить</button>
-    </div>`
-  ).join('');
-}
-
-render();
