6fae5716630841987522612f259eaa1752a0862f
alexshn88888-sudo <alexshn88888@gmail.com>
2026-05-22 17:01:04 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,17 +1,27 @@
 function addProduct() {
   let products = JSON.parse(localStorage.getItem("products")) || [];
 
-  const newProduct = {
-    id: Date.now(),
-    name: document.getElementById("name").value,
-    price: document.getElementById("price").value,
-    description: document.getElementById("desc").value,
-    image: document.getElementById("img").value
+  const file = document.getElementById("img").files[0];
+
+  const reader = new FileReader();
+
+  reader.onload = function () {
+
+    const newProduct = {
+      id: Date.now(),
+      name: document.getElementById("name").value,
+      price: document.getElementById("price").value,
+      description: document.getElementById("desc").value,
+      image: reader.result
+    };
+
+    products.push(newProduct);
+    localStorage.setItem("products", JSON.stringify(products));
+
+    alert("Товар добавлен!");
   };
 
-  products.push(newProduct);
-
-  localStorage.setItem("products", JSON.stringify(products));
-
-  alert("Товар добавлен!");
+  if (file) {
+    reader.readAsDataURL(file);
+  }
 }
