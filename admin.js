3405e8e3467ec4a4f35ced2c99a1057e396a3e69
alexshn88888-sudo <alexshn88888@gmail.com>
2026-05-23 03:56:20 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,9 @@
-const SUPABASE_URL = "https://ombhbcsjwwnvqcwwytql.supabase.co";
 
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tYmhiY3Nqd3dudnFjd3d5dHFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NDU4MzQsImV4cCI6MjA5NTAyMTgzNH0.3y6c1iMR3wHZejYry9GvktO7g2rRUVXmdIu8aLhkdO8";
+const SUPABASE_URL =
+  "https://ombhbcsjwwnvqcwwytql.supabase.co";
+
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tYmhiY3Nqd3dudnFjd3d5dHFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NDU4MzQsImV4cCI6MjA5NTAyMTgzNH0.3y6c1iMR3wHZejYry9GvktO7g2rRUVXmdIu8aLhkdO8";
 
 const client = supabase.createClient(
   SUPABASE_URL,
@@ -18,61 +21,41 @@ async function addProduct() {
   const description =
     document.getElementById("desc").value;
 
-  const file =
-    document.getElementById("img").files[0];
+  // ПРОСТАЯ КАРТИНКА ПО УМОЛЧАНИЮ
+  const image =
+    "https://images.unsplash.com/photo-1544943910-4c1dc44aab44";
 
-  // ЕСЛИ ФОТО НЕ ВЫБРАНО
-  if (!file) {
-
-    const { error } = await client
-      .from("products")
-      .insert([
-        {
-          name,
-          price,
-          description,
-          image: "",
-          category: "fish"
-        }
-      ]);
-
-    if (error) {
-      console.log(error);
-      alert("Ошибка добавления товара");
-      return;
-    }
-
-    alert("Товар добавлен!");
+  // ПРОВЕРКА
+  if (!name || !price) {
+    alert("Заполни название и цену");
     return;
   }
 
-  // ЕСЛИ ЕСТЬ ФОТО
-  const reader = new FileReader();
+  // ДОБАВЛЕНИЕ
+  const { error } = await client
+    .from("products")
+    .insert([
+      {
+        name,
+        price,
+        description,
+        image,
+        category: "fish"
+      }
+    ]);
 
-  reader.onload = async function (e) {
+  // ОШИБКА
+  if (error) {
+    console.log(error);
+    alert("Ошибка добавления");
+    return;
+  }
 
-    const imageBase64 = e.target.result;
+  // УСПЕХ
+  alert("Товар успешно добавлен!");
 
-    const { error } = await client
-      .from("products")
-      .insert([
-        {
-          name,
-          price,
-          description,
-          image: imageBase64,
-          category: "fish"
-        }
-      ]);
-
-    if (error) {
-      console.log(error);
-      alert("Ошибка добавления товара");
-      return;
-    }
-
-    alert("Товар добавлен!");
-  };
-
-  reader.readAsDataURL(file);
+  // ОЧИСТКА
+  document.getElementById("name").value = "";
+  document.getElementById("price").value = "";
+  document.getElementById("desc").value = "";
 }
