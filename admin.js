838765a5c3b03cfcdcd868a3705ecb6cece5b238
alexshn88888-sudo <alexshn88888@gmail.com>
2026-05-23 03:33:02 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://ombhbcsjwwnvqcwwytql.supabase.co";
 
-const SUPABASE_KEY = "ВСТАВЬ_СВОЙ_ANON_KEY";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tYmhiY3Nqd3dudnFjd3d5dHFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NDU4MzQsImV4cCI6MjA5NTAyMTgzNH0.3y6c1iMR3wHZejYry9GvktO7g2rRUVXmdIu8aLhkdO8";
 
 const client = supabase.createClient(
   SUPABASE_URL,
