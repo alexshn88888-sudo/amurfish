03470028fc2837da76549ac30a370d0c2b221320
alexshn88888-sudo <alexshn88888@gmail.com>
2026-05-23 03:23:58 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,22 +1,38 @@
-function addProduct() {
-  let products = JSON.parse(localStorage.getItem("products")) || [];
+const SUPABASE_URL = "https://ombhbcsjwwnvqcwwytql.supabase.co";
 
-  const file = document.getElementById("img").files[0];
+const SUPABASE_KEY = "ВСТАВЬ_СВОЙ_ANON_KEY";
+
+const client = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+async function addProduct() {
+
+  const file =
+    document.getElementById("img").files[0];
 
   const reader = new FileReader();
 
-  reader.onload = function () {
+  reader.onload = async function () {
 
-    const newProduct = {
-      id: Date.now(),
+    const product = {
       name: document.getElementById("name").value,
       price: document.getElementById("price").value,
       description: document.getElementById("desc").value,
-      image: reader.result
+      image: reader.result,
+      category: "fish"
     };
 
-    products.push(newProduct);
-    localStorage.setItem("products", JSON.stringify(products));
+    const { error } = await client
+      .from("products")
+      .insert([product]);
+
+    if (error) {
+      console.log(error);
+      alert("Ошибка");
+      return;
+    }
 
     alert("Товар добавлен!");
   };
